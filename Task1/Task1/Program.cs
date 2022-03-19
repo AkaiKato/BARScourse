c5f5d30c1938c1dbe9209d58b759ea9b38b8b293
AkaiKato <rail201@yandex.ru>
2022-03-19 14:04:10 +0300

    end Task to BARS course

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task work = new Task();
+            work.OnKeyPressed += printMessage;
+            work.Run();
+
+            void printMessage(Object sender, char letter)
+            {
+                Console.WriteLine($"\nYour symbol is {letter}");
+            }
+        }
+    }
+    public class Task
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Press the button:");
+                var letter = Console.ReadKey();
+                
+                switch (letter.KeyChar)
+                {
+                    case 'C':
+                        flag = false;
+                        break;
+                    case 'c':
+                        flag = false;
+                        break;
+                    default:
+                        OnKeyPressed?.Invoke(this, letter.KeyChar);
+                        break;
+                }
+
+            }
+        }
+    }
+}
